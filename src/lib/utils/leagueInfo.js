9549cab313515ea16c757edde7f5af2377332ebd
cmglezen <cmglezen@gmail.com>
2024-12-09 15:40:05 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -56,7 +56,7 @@ export const managers = [
    {
     //   "roster": 3,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
        "managerID": "572985841799827456",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
-       "name": "Chad Glezene",
+       "name": "Chad Glezen",
     //   "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
         "location": "Kansas City", // (optional)
     //   "bio": "Lorem ipsum...",
